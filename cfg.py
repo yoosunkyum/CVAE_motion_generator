4a79a98a2bb383182ddb087559ba3c7b8bbaeb28
yoosunkyum <89465476+yoosunkyum@users.noreply.github.com>
2025-05-21 17:45:56 +0900

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,21 +1,34 @@
-# ========= H1 하이퍼파라미터 및 구조 설정 =========
-input_dim = (19 + 19) + 1*(3 + 4 + 3 + 3)   
-# dof_positions + dof_velocities + body_positions + body_rotations + body_linear_velocities + body_angular_velocities
+# # ========= H1 하이퍼파라미터 및 구조 설정 =========
+# joint_pos_dim = 19
+# joint_vel_dim = joint_pos_dim
+
+# base_pos_dim = 3
+# base_rot_dim = 4
+# base_lin_vel_dim = 3
+# base_ang_vel_dim = 3
+
+# ========= G1 하이퍼파라미터 및 구조 설정 =========
+joint_pos_dim = 29
+joint_vel_dim = joint_pos_dim
+
+base_pos_dim = 3
+base_rot_dim = 4
+base_lin_vel_dim = 3
+base_ang_vel_dim = 3
+
+# ============================================
+#VAE structure related
+input_dim = (joint_pos_dim + joint_vel_dim)\
+            +base_pos_dim + base_rot_dim + base_lin_vel_dim + base_ang_vel_dim   
 cond_dim = input_dim    # s_t_1 차원 (같은 구조라고 가정)
 latent_dim = 64
-hidden_dims = [256, 128]
-num_epochs = 100000
-batch_size = 4096
-learning_rate = 1e-3
-beta = 0.002
-
-# # ========= G1 하이퍼파라미터 및 구조 설정 =========
-# input_dim = (29 + 29) + 1*(3 + 4 + 3 + 3)   
-# # dof_positions + dof_velocities + body_positions + body_rotations + body_linear_velocities + body_angular_velocities
-# cond_dim = input_dim    # s_t_1 차원 (같은 구조라고 가정)
-# latent_dim = 64
-# hidden_dims = [256, 128]
-# num_epochs = 50000
+hidden_dims = [256, 256]
+num_epochs = 10000
+batch_size = 16
 # batch_size = 4096
-# learning_rate = 1e-3
-# beta = 0.002
+
+#learning gains
+learning_rate = 1e-3
+lr_max = 1e-3
+lr_min = 1e-7
+beta = 0.002
