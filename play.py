f9ed3dff5d9d2d37f15b04dd80c77e6792ae446c
yoosunkyum <89465476+yoosunkyum@users.noreply.github.com>
2025-05-22 19:47:45 +0900

    Update play.py

--- play.py
+++ play.py
@@ -7,6 +7,13 @@ from model.cVAE import *
 import time
 from utils import *
 
+import argparse, sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', help=' : .npz file names in motion/ (ignore typing .npz)')
+args = parser.parse_args()
+
+
 def load_trained_decoder(model_path, input_dim, cond_dim, hidden_dims, latent_dim):
     # decoder = Decoder(output_dim=input_dim, cond_dim=cond_dim, hidden_dims=hidden_dims[::-1], latent_dim=latent_dim)
     decoder = MixedDecoder(input_dim, cond_dim, hidden_dims[0], latent_dim, 6)
@@ -24,6 +31,8 @@ def visualize_in_mujoco_with_trained_decoder(model_path, xml_path, s_0, input_di
 
     with viewer.launch_passive(m, d) as v:
         s_prev = s_0.unsqueeze(0)
+        d.qpos[0]=0.0
+        d.qpos[1]=0.0
 
         while True:
             z = torch.randn(1, latent_dim)
@@ -31,18 +40,19 @@ def visualize_in_mujoco_with_trained_decoder(model_path, xml_path, s_0, input_di
             with torch.no_grad():
                 s_curr = decoder(z, s_prev)
                 
-            # s_curr_np = s_curr[0].cpu().numpy()
+            s_curr_np = s_curr[0].cpu().numpy()
             
-            s_curr_lpf = lpf(s_curr, s_prev, 0.5)
-            s_curr_np = s_curr_lpf[0].cpu().numpy()
-            
+            # s_curr_lpf = lpf(s_curr, s_prev, 0.5)
+            # s_curr_np = s_curr_lpf[0].cpu().numpy()            
             
             body_pos = s_curr_np[joint_pos_dim + joint_vel_dim 
                                 :joint_pos_dim + joint_vel_dim + base_pos_dim]
-            body_quat = s_curr_np[joint_pos_dim + joint_vel_dim + base_pos_dim 
-                                 :joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim]
-
-            d.qpos[:3] = body_pos
+            # body_quat = s_curr_np[joint_pos_dim + joint_vel_dim + base_pos_dim 
+            #                      :joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim]
+            body_quat = rot6d2quat(torch.Tensor([s_curr_np[joint_pos_dim + joint_vel_dim + base_pos_dim 
+                                 :joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim]]))
+            # d.qpos[:3] = body_pos
+            d.qpos[2]=body_pos[2]
             d.qpos[3:7] = body_quat
             d.qpos[7:] = s_curr_np[0:joint_pos_dim]
             
@@ -51,6 +61,8 @@ def visualize_in_mujoco_with_trained_decoder(model_path, xml_path, s_0, input_di
             d.qvel[3:6] = s_curr_np[joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim + base_lin_vel_dim
                                    :joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim + base_lin_vel_dim + base_ang_vel_dim]
             d.qvel[6:] = s_curr_np[joint_pos_dim : joint_pos_dim + joint_vel_dim]
+            
+            d.qpos[:2] += d.qvel[:2] * 1/30
 
             mujoco.mj_step(m, d)
             v.sync()
@@ -61,21 +73,23 @@ def visualize_in_mujoco_with_trained_decoder(model_path, xml_path, s_0, input_di
 
         print("시각화 완료. ESC로 종료.")
 
-if __name__ == "__main__":
-    folder_path = "output/g1_boxing"
+def main(argv, args):
+    FILENAME = args.file
+    folder_path = f"output/{FILENAME}"
     
-    model_path = f"{folder_path}/cvae_model.pth"
+    model_path = f"{folder_path}/cvae_model.pt"
     cfg = np.load(f"{folder_path}/cfg.npz","rb")
 
     # xml_path = "assets/h1/h1.xml"
     xml_path = "assets/g1/g1_29dof_rev_1_0.xml"
     
     
-    motion = np.load("motion/g1_boxing.npz","rb")
+    motion = np.load(f"motion/{FILENAME}.npz","rb")
     s_t = torch.cat([torch.Tensor(motion["dof_positions"]),
                      torch.Tensor(motion["dof_velocities"]),
                      torch.flatten(torch.Tensor(motion["body_positions"][:,0,:]), 1),
-                     torch.flatten(torch.Tensor(motion["body_rotations"][:,0,:]), 1),
+                    #  torch.flatten(torch.Tensor(motion["body_rotations"][:,0,:]), 1),
+                     quat2rot6d(torch.flatten(torch.Tensor(motion["body_rotations"][:,0,:]), 1)), # includes conversion from quat to rot6d for continuity
                      torch.flatten(torch.Tensor(motion["body_linear_velocities"][:,0,:]), 1),
                      torch.flatten(torch.Tensor(motion["body_angular_velocities"][:,0,:]), 1)], dim=1)
     s_0 = s_t[0,:]
@@ -90,3 +104,7 @@ if __name__ == "__main__":
         hidden_dims=cfg["hidden_dims"],
         latent_dim=cfg["latent_dim"],
     )
+    
+if __name__ == "__main__":
+    argv = sys.argv
+    main(argv, args)
