2d1826d3b98f9d9805a45605fac013f766f90a5d
yoosunkyum <89465476+yoosunkyum@users.noreply.github.com>
2025-05-26 17:35:32 +0900

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,100 +1,36 @@
-import mujoco
-from mujoco import viewer
-import numpy as np
-import torch
-from cfg import *
-from model.cVAE import * 
-import time
-from utils import *
+# # ========= H1 하이퍼파라미터 및 구조 설정 =========
+# joint_pos_dim = 19
+# joint_vel_dim = joint_pos_dim
 
-import argparse, sys
+# base_pos_dim = 3
+# base_rot_dim = 4
+# base_lin_vel_dim = 3
+# base_ang_vel_dim = 3
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--file', help=' : .npz file names in motion/ (ignore typing .npz)')
-args = parser.parse_args()
+# ========= G1 하이퍼파라미터 및 구조 설정 =========
+joint_pos_dim = 29
+joint_vel_dim = joint_pos_dim
 
+# base_pos_dim = 3
+base_pos_dim = 1 #absolute x,y position은 사용하지 않을 경우
+# base_rot_dim = 4
+base_rot_dim = 6 # rot6d 사용
+base_lin_vel_dim = 3
+base_ang_vel_dim = 3
 
-def load_trained_decoder(model_path, input_dim, cond_dim, hidden_dims, latent_dim):
-    # decoder = Decoder(output_dim=input_dim, cond_dim=cond_dim, hidden_dims=hidden_dims[::-1], latent_dim=latent_dim)
-    decoder = MixedDecoder(input_dim, cond_dim, hidden_dims[0], latent_dim, 6)
-    checkpoint = torch.load(model_path, map_location=torch.device('cpu'))
-    decoder.load_state_dict(checkpoint['decoder'])
-    decoder.eval()
-    return decoder
+# ============================================
+#VAE structure related
+input_dim = (joint_pos_dim + joint_vel_dim)\
+            +base_pos_dim + base_rot_dim + base_lin_vel_dim + base_ang_vel_dim   
+cond_dim = input_dim    # s_t_1 차원 (같은 구조라고 가정)
+latent_dim = 64
+hidden_dims = [256, 256]
+num_epochs = 10000
+# batch_size = 512
+batch_size = 4096
 
-def visualize_in_mujoco_with_trained_decoder(model_path, xml_path, s_0, input_dim, cond_dim, hidden_dims, latent_dim):
-    decoder = load_trained_decoder(model_path, input_dim, cond_dim, hidden_dims, latent_dim)
-
-    # Mujoco 초기화
-    m = mujoco.MjModel.from_xml_path(xml_path)
-    d = mujoco.MjData(m)
-
-    with viewer.launch_passive(m, d) as v:
-        s_prev = s_0.unsqueeze(0)
-        d.qpos[0]=0.0
-        d.qpos[1]=0.0
-
-        while True:
-            z = torch.randn(1, latent_dim)
-            # print(z.size())
-            with torch.no_grad():
-                s_curr = decoder(z, s_prev)
-                
-            s_curr_np = s_curr[0].cpu().numpy()
-            
-            # s_curr_lpf = lpf(s_curr, s_prev, 0.5)
-            # s_curr_np = s_curr_lpf[0].cpu().numpy()            
-            
-            body_pos = s_curr_np[joint_pos_dim + joint_vel_dim 
-                                :joint_pos_dim + joint_vel_dim + base_pos_dim]
-            # body_quat = s_curr_np[joint_pos_dim + joint_vel_dim + base_pos_dim 
-            #                      :joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim]
-            body_quat = rot6d2quat(torch.Tensor([s_curr_np[joint_pos_dim + joint_vel_dim + base_pos_dim 
-                                 :joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim]]))
-            d.qpos[:3] = body_pos
-            # d.qpos[2]=body_pos
-            d.qpos[3:7] = body_quat
-            d.qpos[7:] = s_curr_np[0:joint_pos_dim]
-            
-            d.qvel[:3] = s_curr_np[joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim 
-                                  :joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim + base_lin_vel_dim]
-            d.qvel[3:6] = s_curr_np[joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim + base_lin_vel_dim
-                                   :joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim + base_lin_vel_dim + base_ang_vel_dim]
-            d.qvel[6:] = s_curr_np[joint_pos_dim : joint_pos_dim + joint_vel_dim]
-            
-            d.qpos[:2] += d.qvel[:2] * 1/30
-
-            mujoco.mj_step(m, d)
-            v.sync()
-
-            s_prev = s_curr
-            time.sleep(1/30)
-            
-
-        print("시각화 완료. ESC로 종료.")
-
-def main(argv, args):
-    FILENAME = args.file
-    folder_path = f"output/{FILENAME}"
-    
-    model_path = f"{folder_path}/cvae_model.pt"
-    cfg = np.load(f"{folder_path}/cfg.npz","rb")
-
-    # xml_path = "assets/h1/h1.xml"
-    xml_path = "assets/g1/g1_29dof_rev_1_0.xml"
-  
-    s_0 = torch.Tensor(cfg["s_0"])
-
-    visualize_in_mujoco_with_trained_decoder(
-        model_path=model_path,
-        xml_path=xml_path,
-        s_0=s_0,
-        input_dim=cfg["input_dim"],
-        cond_dim=cfg["cond_dim"],
-        hidden_dims=cfg["hidden_dims"],
-        latent_dim=cfg["latent_dim"],
-    )
-    
-if __name__ == "__main__":
-    argv = sys.argv
-    main(argv, args)
+#learning gains
+learning_rate = 1e-3
+lr_max = 1e-3
+lr_min = 1e-7
+beta = 10.0
