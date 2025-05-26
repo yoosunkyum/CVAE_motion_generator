4a07343e922e388a2a6f85e497e74ea0aeb98dc7
yoosunkyum <89465476+yoosunkyum@users.noreply.github.com>
2025-05-26 17:34:31 +0900

    Update play.py

--- play.py
+++ play.py
@@ -10,7 +10,8 @@ from utils import *
 import argparse, sys
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--file', help=' : .npz file names in motion/ (ignore typing .npz)')
+parser.add_argument('--folder', help=' : .npz file names in motion/ (ignore typing .npz)')
+parser.add_argument('--period', help=' : model number to load')
 args = parser.parse_args()
 
 
@@ -28,13 +29,25 @@ def visualize_in_mujoco_with_trained_decoder(model_path, xml_path, s_0, input_di
     # Mujoco 초기화
     m = mujoco.MjModel.from_xml_path(xml_path)
     d = mujoco.MjData(m)
+    
+    target_body = "pelvis"    
+    body_id = mujoco.mj_name2id(m, mujoco.mjtObj.mjOBJ_BODY, target_body)
 
     with viewer.launch_passive(m, d) as v:
+        v.cam.type = mujoco.mjtCamera.mjCAMERA_TRACKING
+        v.cam.trackbodyid = body_id
+        v.cam.distance = 5.0
+        v.cam.elevation = -30
+        v.cam.azimuth = 90
+        
         s_prev = s_0.unsqueeze(0)
         d.qpos[0]=0.0
         d.qpos[1]=0.0
-
+        # i = 0
+        # z = torch.randn(1, latent_dim)
         while True:
+            # i+=1
+            # if (i % 30 == 0):
             z = torch.randn(1, latent_dim)
             # print(z.size())
             with torch.no_grad():
@@ -51,9 +64,11 @@ def visualize_in_mujoco_with_trained_decoder(model_path, xml_path, s_0, input_di
             #                      :joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim]
             body_quat = rot6d2quat(torch.Tensor([s_curr_np[joint_pos_dim + joint_vel_dim + base_pos_dim 
                                  :joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim]]))
-            d.qpos[:3] = body_pos
-            # d.qpos[2]=body_pos
+            # d.qpos[:3] = body_pos
+            d.qpos[2]=body_pos
+
             d.qpos[3:7] = body_quat
+            
             d.qpos[7:] = s_curr_np[0:joint_pos_dim]
             
             d.qvel[:3] = s_curr_np[joint_pos_dim + joint_vel_dim + base_pos_dim + base_rot_dim 
@@ -63,6 +78,8 @@ def visualize_in_mujoco_with_trained_decoder(model_path, xml_path, s_0, input_di
             d.qvel[6:] = s_curr_np[joint_pos_dim : joint_pos_dim + joint_vel_dim]
             
             d.qpos[:2] += d.qvel[:2] * 1/30
+            
+            # d.qpos[3:7] =integrate_quaternion(torch.Tensor(d.qpos[3:7]),torch.Tensor(d.qvel[3:6]),1/30)
 
             mujoco.mj_step(m, d)
             v.sync()
@@ -74,10 +91,10 @@ def visualize_in_mujoco_with_trained_decoder(model_path, xml_path, s_0, input_di
         print("시각화 완료. ESC로 종료.")
 
 def main(argv, args):
-    FILENAME = args.file
-    folder_path = f"output/{FILENAME}"
+    FOLDER = args.folder
+    folder_path = f"output/{FOLDER}"
     
-    model_path = f"{folder_path}/cvae_model.pt"
+    model_path = f"{folder_path}/cvae_model_{args.period}.pt"
     cfg = np.load(f"{folder_path}/cfg.npz","rb")
 
     # xml_path = "assets/h1/h1.xml"
